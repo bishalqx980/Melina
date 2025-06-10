7e26caa969fe74e378ebdc31da9a9cd39e07f2e7
Bishal Hasan Bhuiyan <bishalqx680@gmail.com>
2025-06-10 22:45:20 +0600

    Added: /info cmd

--- app/handlers/info.py
+++ app/handlers/info.py
@@ -4,20 +4,41 @@ from pyrogram.enums import ChatType
 from app import app
 from app.helpers import BuildKeyboard
 
-class HelpMenuData:
-    TEXT = (
-        "<blockquote>Help Menu</blockquote>\n\n"
-        "• /id - See user/chat ID\n"
-        "• /info - See user info\n"
-        "• /tagall - Tag all chat members\n"
-        "• /stoptagall - Stop tagging chat members\n"
-        "• /log - [debug] (owner only)\n"
+@app.on_message(filters.command("info", ["/", "-", "!", "."]))
+async def func_info(client, message: Message):
+    user = message.from_user
+    reply = message.reply_to_message
+    replied_user = reply.from_user if reply else None
+    args = " ".join(message.command[1:])
+
+    victim = None # get info of
+    fetch_info = False
+
+    if not replied_user and not args:
+        victim = user
+    else:
+        if args: fetch_info = True
+        victim = args or replied_user
+    
+    if fetch_info:
+        user_info = await app.get_users(victim)
+    else:
+        user_info = victim
+
+    async for photo in app.get_chat_photos(user_info.id, 1):
+        photo = photo.file_id or None
+    
+    text = (
+        f"• Name: {user_info.full_name}\n"
+        f"  - First: {user_info.first_name}\n"
+        f"  - Last: {user_info.last_name}\n"
+        f"• Username: {user_info.username}\n"
+        f"• ID: `{user_info.id}`\n"
+        f"• DC: `{user_info.dc_id}`\n"
+        f"• Lang: {user_info.language_code}"
     )
 
-    BUTTONS = [{"Close ✖️": "help_close"}]
-
-
-@app.on_message(filters.command("help", ["/", "-", "!", "."]))
-async def func_help(client, message: Message):
-    keyboard = BuildKeyboard.cbutton(HelpMenuData.BUTTONS)
-    await message.reply_text(HelpMenuData.TEXT, reply_markup=keyboard)
+    if photo:
+        await message.reply_photo(photo, caption=text)
+    else:
+        await message.reply_text(text)
